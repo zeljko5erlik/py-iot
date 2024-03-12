aea5e9c139e4397fe55830a03761d5000bb08cfc
zeljko5erlik <zeljko.peterlik@gmail.com>
2024-03-12 20:18:56 +0000

    humidity color change

--- py_metero_03.py
+++ py_metero_03.py
@@ -4,8 +4,18 @@ import time
 hat = SenseHat()
 hat.clear()
 
-def get_led_matrix_picture(color_up, color_down, humidity):
+def get_led_matrix_picture(humidity):
     led_matrix = []
+    color_down = [0, 0, 0]
+    g = [0, 255, 0]
+    y = [255, 255, 0]
+    r = [255, 0, 0]
+    if humidity < 45:
+        color_up = g
+    elif humidity > 45 and humidity < 75:
+        color_up = y
+    else:
+        color_up = r
 
     for i in range(64):
         if i < int((64/100) * humidity):
@@ -15,11 +25,9 @@ def get_led_matrix_picture(color_up, color_down, humidity):
 
     return led_matrix
 
-b = [0, 0, 0]
-w = [255, 255, 255]
 
 while True:
     humidity = round(hat.get_humidity(), 2)
-    led_matrix = get_led_matrix_picture(w, b, humidity)
+    led_matrix = get_led_matrix_picture(humidity)
     hat.set_pixels(pixel_list=led_matrix)
     time.sleep(1)
